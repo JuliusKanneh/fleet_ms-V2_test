713117c9ab7748f99a04eb490c20de0b658c8bbe
JuliusKanneh <juliuskanneh7@gmail.com>
2022-01-13 20:14:38 +0200

    First commit

---
+++ src/main/java/com/wisdom/fleet_msV2/FleetMsV2Application.java
@@ -0,0 +1,13 @@
+package com.wisdom.fleet_msV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetMsV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetMsV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/wisdom/fleet_msV2/FleetMsV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wisdom.fleet_msV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetMsV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
